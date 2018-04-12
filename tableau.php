61745b336555a6e86b6efed7816a52ca5d6a0a02
VANKOCKO <van.kocko@gmail.com>
2018-04-12 07:58:53 +0200

    test

---
+++ tableau.php
@@ -0,0 +1,16 @@
+
+
+<?php 
+    
+   function moyenne()
+   {     
+    $tab= array(1,2,3,4,5);
+    $somme=0;
+    for($i=0;$i<count($tab); $i++)
+    {
+         $somme=$somme + $tab[$i];
+    }
+
+   }
+
+?>
